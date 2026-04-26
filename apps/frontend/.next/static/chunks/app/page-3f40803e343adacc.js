93a1fded44272ae4fecf0e2cb8e9d30c6e58c0c2
RISHABH KUMAR <mannu2002@outlook.com>
2026-04-26 19:51:28 +0530

    Clean repository by removing generated frontend artifacts.
    Ignore Next.js build outputs and common build directories so only source files are versioned.

--- apps/frontend/.next/static/chunks/app/page-3f40803e343adacc.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[974],{8519:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,3483,23))}},e=>{e.O(0,[483,575,695,358],()=>e(e.s=8519)),_N_E=e.O()}]);
